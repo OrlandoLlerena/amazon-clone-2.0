7ffa917c77cbd79c097b9d700dac6b935c64f910
orlandollerena <trancetribal@bellsouth.net>
2021-11-19 20:03:11 -0500

    icon and searchbar done

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,11 +1,16 @@
 import Image from "next/image";
+import {
+  MenuIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
 
 function Header() {
   return (
-    <header>
+    <header className="sticky top-0 z-50">
       {/* main navbar */}
-      {/* Amazon - Icon */}
-      <div className="flex items-center bg-amazon_blue p-1 py-2 flex-grow">
+      {/* Icon section */}
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
         <div className="mt-2 flex item-center flex-grow sm:flex-grow-0">
           <Image
             src="https://links.papareact.com/f90"
@@ -15,17 +20,33 @@ function Header() {
             className="cursor-pointer"
           />
         </div>
-        {/* serchbar */}
-        <div>
-          <input type="text" />
+
+        {/* searchbar section*/}
+        <div className="hidden sm:flex h-10 items-center rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500 ">
+          <input
+            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4"
+            type="text"
+          />
+          <SearchIcon className="h-12 p-4" />
+        </div>
+
+        {/* shop section */}
+        <div className="text-white">
+          <div>
+            <p>Hello Orlando Llerena</p>
+            <p>Account & Lists</p>
+          </div>
+          <div>
+            <p>More things here</p>
+          </div>
+          <div>
+            <p>cart info here </p>
+          </div>
         </div>
       </div>
 
       {/* secondary navbar */}
-
-      <div>
-        <h1>Im the header</h1>
-      </div>
+      <div></div>
     </header>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,10 @@ import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon Clone 2.0</title>
       </Head>
-
-      {/* Header */}
       <Header />
 
       {/* Banner companent */}
